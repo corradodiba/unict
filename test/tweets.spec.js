7c3de9a4ed4f517d3fdd9dc79289de9537568f93
Davide Turzo <turzodavide@MacBook-Pro-4.homenet.telecomitalia.it>
2020-05-30 17:51:12 +0200

    Work in progress

--- test/tweets.spec.js
+++ test/tweets.spec.js
@@ -3,3 +3,12 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
+
+describe('GET: /users', () => {
+    it('Empty Array if no tweets are found', async () => {
+        const result = await chai.request(app).get('/tweets');
+        expectJson(result);
+        expect(result.body).to.be.instanceOf(Array);
+        expect(result.body).to.has.lengthOf(0);
+    })
+});
