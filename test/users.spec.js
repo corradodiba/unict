3175067e3ad53b338650bdf60c1aac21b22f233c
Giusy Giangravè <49571332+GlitchXCIV@users.noreply.github.com>
2020-05-30 14:18:44 +0200

    test users

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,5 +1,18 @@
-const expect = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app');
 
 chai.use(chaiHttp);
+
+describe('GET /', (
+
+  it('test users', async() => {   
+    const result = await chai.request(app).get('/users');
+    expect(result.status).to.be.equal(200);
+  })
+
+  
+
+));
+
